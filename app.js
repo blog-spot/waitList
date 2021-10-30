16966ea4505cbdfc8f28afa90470d52e433475ec
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-30 15:05:10 +0530

    waitlist-databse up

--- app.js
+++ app.js
@@ -33,14 +33,9 @@ app.post('/submit' , function(req,res) {
     connnetion.query("INSERT INTO waiters (email)  VALUES ('"+email+"')"), function(err , result) {
         if(err)
             throw err;
-        
-
     }
-    fs.readFile('./views/waitList-added.ejs' , function(err,data){
-        res.writeHead(200, {'Context-type': 'text/html'});
-        res.write(data);
-        return res.end();
-      })
+    res.end()
+ 
 });
 
 app.listen(port, () => {
