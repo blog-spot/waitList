88200904559562cd6f897d2af43f84dfd5d82c89
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-30 15:20:51 +0530

    conncet static files. and tested the stactic files

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js' , express.static(__dirname + 'public/js'))
 
 // connceting to the waitlist databse
 
