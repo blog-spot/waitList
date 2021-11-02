7ea7427e1dab43b86a9a5997a79c70a3740771a5
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-02 14:26:04 +0530

    removing firebase

--- app.js
+++ app.js
@@ -5,30 +5,6 @@ const port = process.env.port || 3000;
 const fs = require('fs')
 const mysql = require('mysql')
 const bodyParser = require('body-parser');
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAjj2kLmohkznSmQ_QOO1oqQF7zeHmgPIM",
-  authDomain: "devgram-109cb.firebaseapp.com",
-  projectId: "devgram-109cb",
-  storageBucket: "devgram-109cb.appspot.com",
-  messagingSenderId: "11976664737",
-  appId: "1:11976664737:web:70ff28b50d66f0e14c32e0",
-  measurementId: "G-H50D2PCX8G"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 // Static Files
@@ -70,6 +46,6 @@ app.post('/submit' , function(req,res) {
 
 
 
-app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+app.listen(process.env.PORT || port, '0.0.0.0', () => {
   console.log(`server is running`)
 });
